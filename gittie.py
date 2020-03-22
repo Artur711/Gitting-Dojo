6bda21f2ffcee7c23c46bb5239c89a1caaccf2ac
ankielbowicz <an.kielbowicz@gmail.com>
2020-03-22 10:27:39 +0100

    add jokes

--- gittie.py
+++ gittie.py
@@ -1,1 +1,10 @@
 print("Hello, I'm Gittie!")
+
+
+def joke():
+    print("Doctor: I'm sorry but you suffer from a terminal illness and have only 10 to live.\
+         Patient: What do you mean, 10? 10 what? Months? Weeks?!\
+             Doctor: Nine.")
+
+
+joke()
